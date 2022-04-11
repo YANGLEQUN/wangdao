936a9bcc53a5f52cafd26ca1541b41ea7efc59bb
Mancuoj <1337381768@qq.com>
2022-04-11 22:52:55 +0800

    feat: update readme & add 12

--- SqList/12.cpp
+++ SqList/12.cpp
@@ -15,32 +15,42 @@ void print_list(SqList &list) {
   cout << endl;
 }
 
-// 生成一个无序不重复列表, [0, len-1]
-SqList init_list(int len) {
-  srand(time(NULL));
+SqList init_list() {
   SqList list;
-  list.length = len;
-  for (int i = 0; i < len; i++) 
-    list.data[i] = i;
-
-  for (int i = len - 1; i >= 0; i--) 
-    swap(list.data[i], list.data[rand() % len]);
-
+  list.length = 8;
+  int data[] = {0, 5, 5, 3, 5, 1, 5, 7};
+  // int data[] = {0, 5, 5, 3, 5, 7, 5, 5};
+  for (int i = 0; i < list.length; i++)
+    list.data[i] = data[i];
   return list;
 }
 
-/***************  2.2.3, 11  ***************/
+/***************  2.2.3, 12  ***************/
+int find_main(SqList &list) {
+  // 1.初始化一个默认值全为0的数组
+  int tmp[list.length] = {0};
 
+  // 2.下标对应元素值, 
+  for (int i = 0; i < list.length; i++) {
+    tmp[list.data[i]]++;
+  }
+
+  // 3.遍历找出主元素
+  for (int i = 0; i < list.length; i++) {
+    if (tmp[i] > list.length / 2 ) 
+      return i;
+  }
+
+  // 4.没找到返回-1
+  return -1;
+}
 /************  22/04/11 Mancuoj  ***********/
 
 int main() {
-  SqList list = init_list(10);
+  SqList list = init_list();
   cout << "原数组: ";
   print_list(list);
 
-
-
-  cout << "修改后: ";
-  print_list(list);
+  cout << "主元素: " << find_main(list) << endl;
   return 0;  
 }
